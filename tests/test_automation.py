096ed9b5471ac5257b410b0a7903804dc9b5f418
Noura-Alquran <nmalquran15@eng.just.edu.jo>
2021-06-02 22:22:21 +0300

    lab 19

--- tests/test_automation.py
+++ tests/test_automation.py
@@ -1,5 +1,8 @@
 from automation import __version__
-
+from automation.automation import extract_phones_and_emails
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_extract_phones_and_emails():
+    pass
